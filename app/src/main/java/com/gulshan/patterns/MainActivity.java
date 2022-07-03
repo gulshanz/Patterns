2665f6098fbddf0744eab61bd3985cd6b0f5fbb4
Gulshan <gulshan@zestiot.io>
2022-07-04 01:07:52 +0530

    MVC Completed

--- app/src/main/java/com/gulshan/patterns/MainActivity.java
+++ app/src/main/java/com/gulshan/patterns/MainActivity.java
@@ -3,12 +3,37 @@ package com.gulshan.patterns;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+        textView = findViewById(R.id.textView);
     }
+
+    public MyModel GetAppFromModel() {
+        return new MyModel("Master coding app", " 9000", 5);
+    }
+
+    public void displayAppText(View view) {
+        MyModel model = GetAppFromModel();
+        textView.setText(model.getAppName() + " " + "Downloads" + model.getAppDownloads());
+    }
+
+
+    //MVC
+    // model: MyModel
+    // view: Textview
+    // controller: MainActivity
+
+    // not used much professionally
 }
